73140fec5d8c0102969cbc6117ad342369a44e01
aycabta <aycabta@gmail.com>
2019-01-13 01:47:02 +0900

    Rename var name "div" with "item"

--- mcs-proguide.com-fixer.user.js
+++ mcs-proguide.com-fixer.user.js
@@ -9,16 +9,16 @@
 // ==/UserScript==
 
 (function() {
-    document.querySelectorAll('td').forEach((div) => {
-        div.style.fontSize = '8pt';
-        div.style.lineHeight = '1.55em';
-        div.style.borderStyle = 'none';
+    document.querySelectorAll('td').forEach((item) => {
+        item.style.fontSize = '8pt';
+        item.style.lineHeight = '1.55em';
+        item.style.borderStyle = 'none';
     });
-    document.querySelectorAll('table').forEach((div) => {
-        div.style.borderStyle = 'none';
+    document.querySelectorAll('table').forEach((item) => {
+        item.style.borderStyle = 'none';
     });
-    document.querySelectorAll('p').forEach((div) => {
-        div.style.fontSize = '8pt';
-        div.style.lineHeight = '1.55em';
+    document.querySelectorAll('p').forEach((item) => {
+        item.style.fontSize = '8pt';
+        item.style.lineHeight = '1.55em';
     });
 })();
